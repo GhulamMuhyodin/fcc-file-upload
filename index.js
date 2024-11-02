810854b15f406373168fdf8c15018faf8d76d249
Ghulam Mahyodin (GM) <gmuhyodin@gmail.com>
2024-11-02 21:17:29 +0000

    File Upload code

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
-
+const multer = require('multer');
 var app = express();
 
+const upload = multer();
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
+
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  res.json({
+      name: req.file.originalname,
+      type: req.file.mimetype,
+      size: req.file.size
+  });
+});
+
+
+
 
 
 
